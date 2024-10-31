41783eeda05092dd8206bd728e6e9c24c4e89a1b
Angel <127758281+iamnxtthis@users.noreply.github.com>
2024-10-31 09:45:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contatoForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    alert("Mensagem enviada! Obrigado por entrar em contato.");
+});
